67624ed2208bec4f2f18055ed1125ec4fbca1ff9
Kajal Kokare <162293149+kokarekajal@users.noreply.github.com>
2024-07-01 16:18:32 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,48 +1,12 @@
 
-TEMPERATURE_LIMITS = (0, 45)
-SOC_LIMITS = (20, 80)
-CHARGE_RATE_LIMIT = 0.8
-
-def check_limit(value, min_limit, max_limit, vital_name):
-    if value < min_limit:
-        return (vital_name, 'low')
-    elif value > max_limit:
-        return (vital_name, 'high')
-    return (vital_name, 'normal')
-
-def check_temperature(temperature):
-    return check_limit(temperature, TEMPERATURE_LIMITS[0], TEMPERATURE_LIMITS[1], 'Temperature')
-
-def check_soc(soc):
-    return check_limit(soc, SOC_LIMITS[0], SOC_LIMITS[1], 'State of Charge')
-
-def check_charge_rate(charge_rate):
-    if charge_rate > CHARGE_RATE_LIMIT:
-        return ('Charge Rate', 'high')
-    return ('Charge Rate', 'normal')
-
+def is_temperature_out_of_range(temperature):
+    return temperature < 0 or temperature > 45
+def is_soc_out_of_range(soc):
+    return soc < 20 or soc > 80
+def is_charge_rate_out_of_range(charge_rate):
+    return charge_rate > 0.8
 def battery_is_ok(temperature, soc, charge_rate):
-    checks = [
-        check_temperature(temperature),
-        check_soc(soc),
-        check_charge_rate(charge_rate)
-    ]
-    
-    abnormal_conditions = [check for check in checks if check[1] != 'normal']
-    
-    if abnormal_conditions:
-        for condition in abnormal_conditions:
-            report_issue(condition[0], condition[1])
-        return False
-    
-    return True
-
-def report_issue(vital, breach_type):
-    print(f'{vital} is {breach_type}!')
-
+    return not (is_temperature_out_of_range(temperature) or is_soc_out_of_range(soc) or is_charge_rate_out_of_range(charge_rate))
 if __name__ == '__main__':
-    assert(battery_is_ok(25, 70, 0.7) is True)
-    assert(battery_is_ok(50, 85, 0.7) is False)
-    assert(battery_is_ok(25, 15, 0.7) is False)
-    assert(battery_is_ok(-5, 60, 0.7) is False)
-    assert(battery_is_ok(25, 70, 0.85) is False)
+    assert battery_is_ok(25, 70, 0.7) is True
+    assert battery_is_ok(50, 85, 0) is False
